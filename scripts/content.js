4271f31d11aa8d39e34b81120dcf5046d4eb71b5
aladdin4u <taofikakinkumi@gmail.com>
2024-04-03 22:34:41 +0100

    more options

--- scripts/content.js
+++ scripts/content.js
@@ -1,33 +1,251 @@
+const names = ["john", "joe", "cater", "snow"];
+const numbers = ["09012345678", "08012345678", "07012345678", "09112345678"];
+const cities = ["new york", "london", "madrid", "lagos"];
+const states = ["new york", "london", "madrid", "lagos"];
+const country_list = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Anguilla",
+  "Antigua &amp; Barbuda",
+  "Argentina",
+  "Armenia",
+  "Aruba",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bermuda",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia &amp; Herzegovina",
+  "Botswana",
+  "Brazil",
+  "British Virgin Islands",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cambodia",
+  "Cameroon",
+  "Cape Verde",
+  "Cayman Islands",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Congo",
+  "Cook Islands",
+  "Costa Rica",
+  "Cote D Ivoire",
+  "Croatia",
+  "Cruise Ship",
+  "Cuba",
+  "Cyprus",
+  "Czech Republic",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Estonia",
+  "Ethiopia",
+  "Falkland Islands",
+  "Faroe Islands",
+  "Fiji",
+  "Finland",
+  "France",
+  "French Polynesia",
+  "French West Indies",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Gibraltar",
+  "Greece",
+  "Greenland",
+  "Grenada",
+  "Guam",
+  "Guatemala",
+  "Guernsey",
+  "Guinea",
+  "Guinea Bissau",
+  "Guyana",
+  "Haiti",
+  "Honduras",
+  "Hong Kong",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Isle of Man",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jersey",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kuwait",
+  "Kyrgyz Republic",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Macau",
+  "Macedonia",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Montserrat",
+  "Morocco",
+  "Mozambique",
+  "Namibia",
+  "Nepal",
+  "Netherlands",
+  "Netherlands Antilles",
+  "New Caledonia",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palestine",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Puerto Rico",
+  "Qatar",
+  "Reunion",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Pierre &amp; Miquelon",
+  "Samoa",
+  "San Marino",
+  "Satellite",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "South Africa",
+  "South Korea",
+  "Spain",
+  "Sri Lanka",
+  "St Kitts &amp; Nevis",
+  "St Lucia",
+  "St Vincent",
+  "St. Lucia",
+  "Sudan",
+  "Suriname",
+  "Swaziland",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Taiwan",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor L'Este",
+  "Togo",
+  "Tonga",
+  "Trinidad &amp; Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Turks &amp; Caicos",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "Uruguay",
+  "Uzbekistan",
+  "Venezuela",
+  "Vietnam",
+  "Virgin Islands (US)",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
+const addresess = ["1600 Pennsylvanna Avenue", "24 Avenue"];
 (() => {
   const form = document.querySelectorAll("form input");
   if (form) {
     form.forEach((element) => {
       if (element.type === "text") {
-        const variable = typeText();
-        element.value = variable;
+        const firstName = element.name.toLowerCase() == "firstName";
+        const city = element.name.toLowerCase() == "city";
+        const address = element.name.toLowerCase() == "address";
+        const state = element.name.toLowerCase() == "state";
+        const country = element.name.toLowerCase() == "country";
+        if (firstName) {
+          const variable = randomVariable(names);
+          element.value = variable;
+        } else if (city) {
+          const variable = randomVariable(cities);
+          element.value = variable;
+        } else if (address) {
+          const variable = randomVariable(addresess);
+          element.value = variable;
+        } else if (state) {
+          const variable = randomVariable(states);
+          element.value = variable;
+        } else if (country) {
+          const variable = randomVariable(country_list);
+          element.value = variable;
+        }
       } else if (element.type === "number") {
-        const variable = typeNumber();
+        const variable = randomVariable(numbers);
         element.value = variable;
       }
     });
   }
 })();
 
-function typeText() {
-  const arrayElement = ["john", "joe", "cater", "snow"];
-  const random = Math.floor(Math.random() * arrayElement.length);
-  const value = arrayElement[random];
-  return value;
-}
-
-function typeNumber() {
-  const arrayElement = [
-    "09012345678",
-    "08012345678",
-    "07012345678",
-    "09112345678",
-  ];
-  const random = Math.floor(Math.random() * arrayElement.length);
-  const value = arrayElement[random];
+function randomVariable(data) {
+  const random = Math.floor(Math.random() * data.length);
+  const value = data[random];
   return value;
 }
