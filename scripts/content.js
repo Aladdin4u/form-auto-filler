2167eff8664d1eabc1c3d20cb57f5cfecfae5910
aladdin4u <taofikakinkumi@gmail.com>
2024-04-05 07:28:18 +0100

    text area

--- scripts/content.js
+++ scripts/content.js
@@ -5,7 +5,7 @@ const emails = [
   "cater@example.com",
   "snow@example.com",
 ];
-const numbers = ["09012345678", "08012345678", "07012345678", "09112345678"];
+const phones = ["09012345678", "08012345678", "07012345678", "09112345678"];
 const cities = ["new york", "london", "madrid", "lagos"];
 const states = ["new york", "london", "madrid", "lagos"];
 const country_list = [
@@ -218,16 +218,27 @@ const country_list = [
 const addresses = ["1600 Pennsylvanna Avenue", "24 Avenue"];
 
 (() => {
-  const form = document.querySelectorAll("form input");
+  const form = document.querySelector("form");
+
   if (form) {
-    form.forEach((element) => {
+    const formInput = document.querySelectorAll("input");
+    const formTextArea = document.querySelectorAll("form textarea");
+    console.log(formInput);
+    formInput.forEach((element) => {
       if (element.type === "text") {
-        const firstName = element.name.toLowerCase() == "firstName";
-        const city = element.name.toLowerCase() == "city";
-        const address = element.name.toLowerCase() == "address";
-        const state = element.name.toLowerCase() == "state";
-        const country = element.name.toLowerCase() == "country";
-        if (firstName) {
+        const formName = element.name.toLowerCase();
+        const cityPattern = /cit\w*/g;
+        const statePattern = /state\w*/g;
+        const countryPattern = /countr\w*/g;
+        const addressPattern = /address\w*/g;
+        const namePattern = /\w*(-|\s)?name\w*/g;
+        const name = namePattern.test(formName);
+        const city = cityPattern.test(formName);
+        const state = statePattern.test(formName);
+        const country = countryPattern.test(formName);
+        const address = addressPattern.test(formName);
+
+        if (name) {
           const variable = generateRandomVariable(names);
           element.value = variable;
         } else if (city) {
@@ -242,6 +253,8 @@ const addresses = ["1600 Pennsylvanna Avenue", "24 Avenue"];
         } else if (country) {
           const variable = generateRandomVariable(country_list);
           element.value = variable;
+        } else {
+          return;
         }
       } else if (element.type === "email") {
         const variable = generateRandomVariable(emails);
@@ -250,7 +263,18 @@ const addresses = ["1600 Pennsylvanna Avenue", "24 Avenue"];
         const variable = generateRandomColor();
         element.value = variable;
       } else if (element.type === "number") {
-        const variable = generateRandomNumber(element.min, element.max);
+        const variable = generateRandomNumber(
+          element.min,
+          element.max,
+          element.step
+        );
+        element.value = variable;
+      } else if (element.type === "range") {
+        const variable = generateRandomNumber(
+          element.min,
+          element.max,
+          element.step
+        );
         element.value = variable;
       } else if (element.type === "date") {
         const variable = generateRandomDate(element.min, element.max);
@@ -261,7 +285,15 @@ const addresses = ["1600 Pennsylvanna Avenue", "24 Avenue"];
       } else if (element.type === "month") {
         const variable = generateRandomDate(element.min, element.max);
         element.value = variable.slice(0, 7);
+      } else if (element.type === "tel") {
+        const variable = generateRandomPhone(element.pattern);
+        element.value = variable;
       }
+    });
+
+    formTextArea.forEach((element) => {
+      const variable = generateLoremText();
+      element.value = variable;
     });
   }
 })();
@@ -272,17 +304,19 @@ function generateRandomVariable(data) {
   return value;
 }
 
-function generateRandomNumber(min, max) {
+function generateRandomNumber(min, max, step) {
   const start = min ? Number(min) : 0;
   const end = max ? Number(max) : 100;
+  const interval = step ? step : 1;
 
-  const newNumber = Math.floor(Math.random() * (end - start) + start);
+  const newNumber =
+    Math.floor(Math.random() * (end - start) + start) * interval;
 
   return newNumber;
 }
 
 function generateRandomDate(min, max) {
-  const start = min ? new Date(min) : new Date();
+  const start = min ? new Date(min) : new Date(0);
   const end = max ? new Date(max) : new Date();
 
   const date = new Date(
@@ -314,8 +348,111 @@ function generateRandomColor() {
     "f",
   ];
   let color = "#";
+
   for (let i = 0; i < 6; i++) {
     color += haxArray[Math.floor(Math.random() * haxArray.length)];
   }
   return color;
+}
+
+function generateRandomPhone(pattern) {
+  const regex = /`${pattern}`/;
+  let newNumber = "";
+  const numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  for (let i = 0; i < 11; i++) {
+    newNumber += numberArray[Math.floor(Math.random() * numberArray.length)];
+  }
+
+  return newNumber;
+}
+
+function generateLoremText() {
+  const paragraphs = [1, 2, 3, 4, 5];
+  const words = [10, 20, 30, 40, 50];
+  const paragraphLength =
+    paragraphs[Math.floor(Math.random() * paragraphs.length)];
+  const wordLength = words[Math.floor(Math.random() * words.length)];
+  var loremText = "";
+  var wordArray = [
+    "Lorem",
+    "ipsum",
+    "dolor",
+    "sit",
+    "amet",
+    "consectetur",
+    "adipiscing",
+    "elit",
+    "sed",
+    "do",
+    "eiusmod",
+    "tempor",
+    "incididunt",
+    "ut",
+    "labore",
+    "et",
+    "dolore",
+    "magna",
+    "aliqua",
+    "Ut",
+    "enim",
+    "ad",
+    "minim",
+    "veniam",
+    "quis",
+    "nostrud",
+    "exercitation",
+    "ullamco",
+    "laboris",
+    "nisi",
+    "ut",
+    "aliquip",
+    "ex",
+    "ea",
+    "commodo",
+    "consequat",
+    "Duis",
+    "aute",
+    "irure",
+    "dolor",
+    "in",
+    "reprehenderit",
+    "in",
+    "voluptate",
+    "velit",
+    "esse",
+    "cillum",
+    "dolore",
+    "eu",
+    "fugiat",
+    "nulla",
+    "pariatur",
+    "Excepteur",
+    "sint",
+    "occaecat",
+    "cupidatat",
+    "non",
+    "proident",
+    "sunt",
+    "in",
+    "culpa",
+    "qui",
+    "officia",
+    "deserunt",
+    "mollit",
+    "anim",
+    "id",
+    "est",
+    "laborum",
+  ];
+
+  for (let i = 0; i < paragraphLength; i++) {
+    var paragraph = "";
+    for (let j = 0; j < wordLength; j++) {
+      var randomWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+      paragraph += randomWord + " ";
+    }
+    loremText += paragraph.trim() + ". \n";
+  }
+  return loremText;
 }
