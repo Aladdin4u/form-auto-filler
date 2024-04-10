bdc5fe560935ce1012e6286e666b4465f5870804
aladdin4u <taofikakinkumi@gmail.com>
2024-04-10 12:30:28 +0100

    style toast

--- scripts/content.js
+++ scripts/content.js
@@ -816,42 +816,54 @@ function createNotification(value) {
   const bodyElement = document.querySelector("body");
   const containerElement = document.createElement("div");
   const divElement = document.createElement("div");
+  const notifyElement = document.createElement("span");
   const headerElement = document.createElement("span");
   const passwordElement = document.createElement("span");
   const copyElement = document.createElement("button");
 
   containerElement.id = "notification";
-  let containerStyles = `max-width:320px;margin: 4px auto;padding:8px;border-radius:8px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);`;
+  let containerStyles = `position:relative;max-width:356px;background-color:white;margin: 4px auto;padding:8px;border-radius:8px;box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);`;
+  const animateTop = [
+    { top: "-300px", opacity: 0 },
+    { top: 0, opacity: 1 },
+  ];
+  const animateOption = { duration: 400 };
+  containerElement.animate(animateTop, animateOption);
 
   containerElement.style = containerStyles;
   let divStyles = `display:flex;justify-content: space-between;align-items:center;padding:5px;background-color:#f8f8f8;`;
   divElement.style = divStyles;
+  let notifyStyles = `display:none;padding:5px;color:green;text-align:center;font-size:12px;`;
+  notifyElement.id = "hide-notify";
+  notifyElement.style = notifyStyles;
 
   headerElement.textContent = "Password:";
-  passwordElement.id = "text";
+  passwordElement.id = "copy-text";
   passwordElement.textContent = value;
   copyElement.innerHTML = "&#128203;";
-  // copyElement.style = `border-color: #f8f8f8;`;
   copyElement.onclick = async function () {
-    const copyText = document.getElementById("text");
-
+    const copyText = document.getElementById("copy-text");
+    const notifyText = document.getElementById("hide-notify");
+    
     try {
       await navigator.clipboard.writeText(copyText.innerText);
     } catch (error) {
       console.error(error.message);
     }
-    let notify = `password ${copyText.innerText} copied to the clipboard!`;
+    let notify = `password "${copyText.innerText}" copied to the clipboard!`;
 
-    headerElement.textContent += ` ${notify}`;
+    notifyText.textContent = notify;
+    notifyText.style.display = "block";
 
     setTimeout(() => {
       clearNotification();
-    }, 3000);
+    }, 5000);
   };
 
   divElement.appendChild(passwordElement);
   divElement.appendChild(copyElement);
 
+  containerElement.appendChild(notifyElement);
   containerElement.appendChild(headerElement);
   containerElement.appendChild(divElement);
 
