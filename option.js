284bb0ff49dd63d2eaeb1b9c2e764f3f1d1a4e6d
aladdin4u <taofikakinkumi@gmail.com>
2024-04-22 11:00:55 +0100

    edit profile

--- option.js
+++ option.js
@@ -1,32 +1,85 @@
 const params = new URLSearchParams(document.location.search);
 const key = params.get("key");
 
+let storeData = [];
 const tbody = document.getElementById("tbody");
 const template = document.getElementById("table_template");
+const addNewField = document.querySelector("button, .add");
+const closeTab = document.querySelector("button, .cancel");
+const saveProfile = document.querySelector("button, .save");
+const inputName = document.querySelectorAll(".input-name");
+const inputValue = document.querySelectorAll(".input-value");
+const deleteInputField = document.querySelectorAll(".delete");
 
 const datas = await chrome.storage.local.get(key);
+storeData = datas[key];
 
-const elements = new Set();
-for (const data of datas[key]) {
-  const element = template.content.firstElementChild.cloneNode(true);
+function fetchAllData() {
+  const elements = new Set();
+  let i = 1;
+  for (const data of storeData) {
+    const element = template.content.firstElementChild.cloneNode(true);
 
-  let isChecked = "";
-  if (data.type === "checkbox" || data.type === "radio") {
-    if (data.checked) {
-      isChecked = 1;
+    element.id = i;
+    element.querySelector(".input-type").textContent = data.type;
+    element.querySelector(".input-name").value = data.name;
+    element.querySelector(".input-value").type = data.type;
+
+    if (data.type === "checkbox" || data.type === "radio") {
+      element.querySelector(".input-value").checked = data.checked;
+    } else if (data.type === "image") {
+      element.querySelector(".input-value").src = data.src;
     } else {
-      isChecked = 0;
+      element.querySelector(".input-value").value = data.value;
     }
-  } else if (data.src) {
-    isChecked = data.src;
-  } else {
-    isChecked = data.value;
+
+    elements.add(element);
+    i++;
   }
 
-  element.querySelector(".type").textContent = data.type;
-  element.querySelector("#name").value = data.name;
-  element.querySelector("#value").value = isChecked;
-  elements.add(element);
+  tbody.append(...elements);
 }
 
-tbody.append(...elements);
+fetchAllData();
+
+if (inputName) {
+  inputName.forEach((element) => {
+    element.addEventListener("click", () => {
+      const parent = element.closest("tr");
+      element.addEventListener("input", (e) => {
+        console.log(e.target.value);
+        let currValue = storeData.find((data, index) => index + 1 == parent.id);
+        currValue.name = e.target.value;
+      });
+    });
+  });
+}
+
+if (inputValue) {
+  inputValue.forEach((element) => {
+    element.addEventListener("click", () => {
+      const parent = element.closest("tr");
+      element.addEventListener("input", (e) => {
+        let currValue = storeData.find((data, index) => index + 1 == parent.id);
+        currValue.value = e.target.value;
+      });
+    });
+  });
+}
+
+if (deleteInputField) {
+  deleteInputField.forEach((element) => {
+    element.addEventListener("click", () => {
+      const parent = element.closest("tr");
+      storeData = storeData.filter((data, index) => index + 1 != parent.id);
+      tbody.removeChild(parent);
+    });
+  });
+}
+
+if (saveProfile) {
+  saveProfile.addEventListener("click", async () => {
+    await chrome.storage.local.set({ [key]: storeData });
+    alert("Profile successfully saved!");
+  });
+}
