f9ff5f80dfa2ebfba9c5a05dcd67fcba87a9cd02
aladdin4u <taofikakinkumi@gmail.com>
2024-08-21 22:50:45 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    popup: "./src/js/popup.js",
+    option: "./src/js/option.js",
+    serviceWorker: "./src/serviceWorker.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/popup.html",
+      filename: "popup.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
+    }),
+  ],
+};
